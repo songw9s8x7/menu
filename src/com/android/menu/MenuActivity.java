b28c5c1f49aadbab8a6f2ab579ca731b54c5d0d2
frank.wang <frank.wang@zhntd.com>
2014-10-09 12:57:20 +0800

    fistcommit

---
+++ src/com/android/menu/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.android.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.menu.R;
+
+public class MenuActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		   setContentView(R.layout.food);
+	}
+
+}
