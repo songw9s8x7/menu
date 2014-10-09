55f50b850eb2ca7dccce5dc406581334322a8d09
frank.wang <frank.wang@zhntd.com>
2014-10-09 14:10:13 +0800

    secondcommit

--- src/com/android/menu/MenuActivity.java
+++ src/com/android/menu/MenuActivity.java
@@ -2,15 +2,31 @@ package com.android.menu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.example.menu.R;
 
 public class MenuActivity extends Activity{
+	private CheckBox menu[];
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		   setContentView(R.layout.food);
+		   menu[0] = (CheckBox)findViewById(R.id.checkBox1);
+		   menu[0].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+	            @Override
+	            public void onCheckedChanged(CompoundButton buttonView,
+	                    boolean isChecked) {
+	                // TODO Auto-generated method stub
+	                if(isChecked){
+	                    
+	                }else{
+	                   
+	                }
+	            }
+	        }); 
 	}
 
 }
